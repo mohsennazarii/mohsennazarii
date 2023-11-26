2768040e56103a5ec979121bc39cbe5548eac32a
mamaddnet <mamaddnet@gmail.com>
2023-11-26 04:09:40 +0330

    disable smooth-scrooling in chrome browsers

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,38 @@
-
-$(document).ready(function() {
-  AOS.init( {
-    
-  });
+$(document).ready(function () {
+  AOS.init({});
 });
 
-$('a.smooth-scroll')
-.click(function(event) {
-  if (
-    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-    && 
-    location.hostname == this.hostname
-  ) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000, function() {
+const browserInfo = navigator.userAgent;
+isChrome = browserInfo.includes("Chrome");
 
-        var $target = $(target);
-        $target.focus();
-        if ($target.is(":focus")) { 
-          return false;
-        } else {
-          $target.attr('tabindex','-1'); 
-          $target.focus(); 
-        };
-      });
+if (!isChrome) {
+  $("a.smooth-scroll").on("click", function (event) {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          0,
+          function () {
+            var $target = $(target);
+            $target.trigger("focus");
+            if ($target.is(":focus")) {
+              return false;
+            } else {
+              $target.attr("tabindex", "-1");
+              $target.trigger("focus");
+            }
+          }
+        );
+      }
     }
-  }
-});
+  });
+}
